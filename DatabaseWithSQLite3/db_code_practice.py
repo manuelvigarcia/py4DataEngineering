ad03a01f90c73322736c6b09b6ad49abeb665438
Manuel Vicente García Rodríguez <manuelvigarcia@hotmail.com>
2026-06-18 16:48:12 +0200

    Added fourth exercise about querying only department names from Departments table.

--- DatabaseWithSQLite3/db_code_practice.py
+++ DatabaseWithSQLite3/db_code_practice.py
@@ -6,23 +6,15 @@ conn = sqlite3.connect('STAFF.db')
 table_name = 'Departments'
 attribute_list=['DEPT_ID','DEP_NAME','MANAGER_ID','LOC_ID']
 
-#file_path='Departments.csv'
-#df = pd.read_csv(file_path,names=attribute_list)
-#df.to_sql(table_name,conn,if_exists='replace',index=False)
-#print('Table is ready')
-
-new_data_dict = {
-    'DEPT_ID':[9],
-    'DEP_NAME':['Quality Assurance'],
-    'MANAGER_ID':[30010],
-    'LOC_ID':['L0010']
-    }
-new_data=pd.DataFrame(new_data_dict)
-new_data.to_sql(table_name,conn,if_exists='append',index=False)
-
 sql_statement = f"SELECT * FROM {table_name}"
 query_output = pd.read_sql(sql_statement,conn)
 print(sql_statement)
 print(query_output)
+
+sql_statement = f"SELECT DEP_NAME FROM {table_name}"
+query_output = pd.read_sql(sql_statement,conn)
+print(sql_statement)
+print(query_output)
+
 
 conn.close()
